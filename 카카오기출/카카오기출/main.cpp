b4c47f3cb9b266f580e9466cd18eda0853f295f1
Kimyeongho <dudgh9661@gmail.com>
2020-05-08 14:55:58 +0900

    kakao practice

---
+++ 카카오기출/카카오기출/main.cpp
@@ -0,0 +1,20 @@
+
+#include <string>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+int solution(vector<vector<int>> board, vector<int> moves) {
+    //인형을 저장한 stack
+    stack<int> dollStore;
+    //크레인이 내려간 위치에 인형이 있는지 검사한다.
+    for(int i = 0; i < moves.size(); i++){
+        int crainPoint = moves[i];
+        
+        for(int j = 0; j < )
+        
+    }
+    int answer = 0;
+    return answer;
+}
