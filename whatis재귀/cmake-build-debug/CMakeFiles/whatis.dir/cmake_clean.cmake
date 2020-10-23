291ddc1b5c241f0ef8a251f6ab914adc8b34297f
Kimyeongho <dudgh9661@naver.com>
2020-10-23 10:50:17 +0900

    recursive

---
+++ whatis재귀/cmake-build-debug/CMakeFiles/whatis.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/whatis.dir/main.cpp.o"
+  "whatis"
+  "whatis.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/whatis.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
