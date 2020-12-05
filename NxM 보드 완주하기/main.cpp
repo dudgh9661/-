ddbd5dc5d2faa741ee321f3b451c29d1e72e42bc
Kimyeongho <dudgh9661@naver.com>
2020-12-05 10:54:06 +0900

    backtracking 재연습

--- NxM 보드 완주하기/main.cpp
+++ NxM 보드 완주하기/main.cpp
@@ -1,121 +1,104 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 
-#define INF 100000;
 using namespace std;
 
-int N,M;
+int N, M;
+int map[31][31] = { 0 };
+bool visited[31][31] = { false };
 
-int map[31][31] = {0};
-bool visited[31][31] = {false};
+int UD[4] = {-1, 0, 1, 0};
+int LR[4] = {0, -1, 0, 1};
 
-int ud[4] = {0,-1,0,1};
-int lr[4] = {-1,0,1,0};
+int zeroCnt = 0;
+int MIN = 1e9;
 
-int MIN = (int)1e9;
-int cntEle = 0;
-
-vector< pair<int,int> > zero;
-
-bool inRange(int r, int c) { //여기서 방향 바꾸는 조건 만듦
-    return r >= 0 && c >= 0 && r < N && c < M && map[r][c] == 0 && !visited[r][c];
-}
-void initVis() {
+void init() {
+    char input;
     for(int i = 0; i < N; i++) {
         for(int j = 0; j < M; j++) {
-            visited[i][j] = false;
-            map[i][j] = 0;
+            cin >> input;
+            if(input == '*') map[i][j] = 1;
+            else {
+                map[i][j] = 0;
+                zeroCnt++;
+            }
         }
     }
 }
 
-
-void DFS(int row, int col, int pos, int dist, int cnt) {
-    if( dist == cntEle) {
-        MIN = min(MIN, cnt);
-//        cout << "Here" << endl;
-        return;
-    }
-    int nr = row + ud[pos];
-    int nc = col + lr[pos];
-//    cout << "cur : " << row << "," << col << endl;
-
-    if( !inRange(nr,nc)) { //더이상 같은 방향으로 진행 불가할때,
-        for(int i = 0; i < 4; i++) {
-
-            nr = row + ud[i];
-            nc = col + lr[i];
-            if( inRange(nr,nc) ) { //방향을 바꿨을 때 나아갈 수 있다면,
-                visited[nr][nc] = true;
-                DFS(nr, nc, i, dist + 1, cnt + 1);
-                visited[nr][nc] = false;
-            }
-        } //if( map[nr][nc] == 0 && visited[nr][nc] == 0 )
-    }
-    else  {
-        visited[nr][nc] = true;
-        DFS(nr,nc, pos, dist+1, cnt);
-        visited[nr][nc] = false;
-    }
-
+bool inRange(int row, int col) {
+    return row >= 0 && row < N && col >= 0 && col < M && !visited[row][col] && map[row][col] == 0;
 }
 
-int main() {
-    int tc = 1;
-    while( cin >> N >> M ) {
-        MIN = 1e9;
-        initVis();
-        cntEle = 0;
+void DFS(int row, int col, int pos, int cnt, int dist) {
+    if ( dist == zeroCnt ) {
+        MIN = min(cnt, MIN);
+        return;
+    }
+        int nr = row + UD[pos];
+        int nc = col + LR[pos];
 
-        char input;
-        for (int i = 0; i < N; i++) {
-            for (int j = 0; j < M; j++) {
-                cin >> input;
-                if (input == '*') map[i][j] = 1;
-                else {
-                    map[i][j] = 0;
-                    zero.push_back({i, j});
-                    cntEle++;
+        // 갈 수 있는 방향
+        if ( inRange(nr, nc) ) {
+            visited[nr][nc] = true;
+            DFS(nr, nc, pos, cnt, dist + 1);
+            visited[nr][nc] = false;
+        }
+        // 갈 수 없는 방향
+        if( !inRange(nr, nc)) {
+            for(int nextPos = 0; nextPos < 4; nextPos++) {
+                if( nextPos == pos ) continue;
+
+                nr = row + UD[nextPos];
+                nc = col + LR[nextPos];
+                if( inRange(nr,nc) ) {
+                    visited[nr][nc] = true;
+                    DFS(nr, nc, nextPos, cnt + 1, dist + 1);
+                    visited[nr][nc] = false;
                 }
             }
         }
 
-        if (cntEle == 1) {
+}
+int main() {
+
+    int tc = 1;
+    while ( cin >> N >> M ) {
+        fill(&visited[0][0], &visited[30][31], false);
+        fill(&map[0][0], &map[30][31], 0);
+        zeroCnt = 0;
+        MIN = 1e9;
+        init();
+
+
+        if (zeroCnt == 1) {
             cout << "Case " << tc << ": " << 0 << endl;
             tc++;
             continue;
         }
-        for (int i = 0; i < zero.size(); i++) {
-            int r = zero[i].first;
-            int c=  zero[i].second;
-            for(int j = 0; j < 4; j++) {
-                visited[r][c] = true;
-                DFS(r, c, j, 1, 1);
-                visited[r][c] = false;
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (map[i][j] == 0) {
+                    for (int d = 0; d < 4; d++) {
+                        visited[i][j] = true;
+                        DFS(i, j, d, 1, 1);
+                        visited[i][j] = false;
+                    }
+                }
             }
         }
-        if (MIN == (int)1e9) MIN = -1;
-        cout << "Case " << tc << ": " << MIN << '\n';
 
-//
-//        for (int i = 0; i < zero.size(); i++) {
-//            int r = zero[i].first;
-//            int c = zero[i].second;
-//            if( !inRange(r+ud[i], c+lr[i])) continue;
-//            for (int j = 0; j < 4; j++) {
-//                visited[r][c] = true;
-//                DFS(r, c, j, 1, 1);
-//                visited[r][c] = false;
-//            }
-//        }
-//        if (MIN != (int)1e9) {
-//            cout << "hi Case " << tc << ": " << MIN << endl;
-//        }
-//        else cout << "Case " << tc << ": "<< -1 << endl;
+        if (MIN == 1e9) MIN = -1;
+        cout << "Case " << tc << ": " << MIN << endl;
         tc++;
-        zero.clear();
-        zero.resize(0);
+
     }
     return 0;
 }
 
+
+
+//갈 수 없을 때까지 같은 방향으로 계속해서 이동한다.
+//갈 수 없는 방향이 나오면 방향을 바꾼다. ( 범위를 벗어나거나, 이미 방문했던 곳을 방문하거나(?), 장애물인 경우 )
